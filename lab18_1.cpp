8226e3e0c8a0855945a4669f8b49eb2d06467afb
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-02-23 17:29:49 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,25 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect &a, Rect &b){
+	double x = min(a.x + a.w , b.x + b.w) - max(a.x , b.x);
+	double y = min(a.y + a.h , b.y + b.h) - max(a.y , b.y);
+	double z;
+	if (a.x + a.w <= b.x || a.y + a.h <= b.y) z = 0;
+	else if(b.x + b.w <= a.x + a.w && b.y + b.h <= a.y + a.h){
+		x = b.w;
+		y = b.h;
+		z = x*y;
+	}
+	else if(b.x + b.w <= a.x + a.w){
+		x = b.w;
+		z = x*y;
+	}
+	else if(b.y + b.h <= a.y + a.h){
+		y = b.h;
+		z = x*y;
+	}
+	else z = x*y;
 
-
-
+return z;
 }
